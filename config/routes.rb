66022b31438822c23c3c64d10567c7437cbbf743
Rodrigo <rguzman1@miaundes.cl>
2025-03-28 15:34:21 -0300

    commit 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root 'static_pages#home'  # Ruta principal, para la página de inicio
-  get 'about', to: 'static_pages#about'  # Ruta para la página About
+  root "static_pages#home" 
+  get 'about', to: 'static_pages#about' 
+  get "/contact", to: "static_pages#contact"
 
 
   get "up" => "rails/health#show", as: :rails_health_check
